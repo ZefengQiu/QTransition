a6796be413cad798e87960cb9f3cf31d34dcceab
ZefengQiu <z9qiu@uwaterloo.ca>
2018-01-06 08:42:04 -0500

    start left

--- QTransition/Classes/Options/QPageTransition.swift
+++ QTransition/Classes/Options/QPageTransition.swift
@@ -5,4 +5,55 @@
 //  Created by Zefeng Qiu on 2018-01-04.
 //
 
-import Foundation
+import UIKit
+
+public class QPageTransition: QBaseTransition {
+  
+  public var direction: QTransitionConstant.Options.Page = .left
+  
+  override public func animateTransition(using transitionContext: UIViewControllerContextTransitioning, fromVC: UIViewController, fromView: UIView, toVC: UIViewController, toView: UIView) {
+    let duration = self.transitionDuration(using: transitionContext)
+    let containerView = transitionContext.containerView
+    
+    if self.isPresenting {
+      containerView.addSubview(toVC.view)
+    }
+    
+    let presentedFrame = transitionContext.finalFrame(for: toVC)
+    var presentingFrame = presentedFrame
+    var dismissFrame = presentedFrame
+    
+    switch self.direction {
+    case .left:
+      presentingFrame.origin.x = presentedFrame.width
+      dismissFrame.origin.x = -presentedFrame.width
+    case .right:
+      break
+      
+    case .top:
+      break
+      
+    case .bottom:
+      break
+    }
+    
+    let toStartFrame = presentingFrame
+    let toEndFrame = presentingFrame
+    
+    toVC.view.frame = toStartFrame
+    
+    UIView.animate(withDuration: duration,
+                   delay: 0.0,
+                   options: .curveEaseInOut,
+                   animations: {
+                    toVC.view.frame = toEndFrame
+    }, completion: { _ in
+      if transitionContext.transitionWasCancelled {
+        transitionContext.completeTransition(false)
+      }else {
+        transitionContext.completeTransition(true)
+      }
+    })
+  }
+  
+}
